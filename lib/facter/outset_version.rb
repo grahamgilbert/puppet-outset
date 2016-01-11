3607ae00f526c37f2aa91b19a1af47fc5daf5fce
Graham Gilbert <graham@grahamgilbert.com>
2016-01-11 11:07:18 +0000

    I hate ruby:

--- lib/facter/outset_version.rb
+++ lib/facter/outset_version.rb
@@ -2,16 +2,18 @@
 Facter.add(:outset_version) do
   confine :kernel => "Darwin"
   setcode do
+    out = ''
     if File.exist?('/usr/local/outset/outset')
         File.open('/usr/local/outset/outset') do |file|
-        file.each_line do |line|
-          if line.start_with?('__version__')
-              out = line.chomp().sub('__version__ = ', '')
-              out[0]=''
-              out.chomp("'")
-              break
-          end
-        end
+            file.each_line do |line|
+              if line.start_with?('__version__')
+                  out = line.chomp().sub('__version__ = ', '')
+                  out[0]=''
+                  out = out.chomp("'")
+                  break
+              end
+            end
+          out
       end
     else
         "Outset not installed"
