b4387b99c3872c0d7a022dcf4f2281490a3aac24
Graham Gilbert <graham@grahamgilbert.com>
2016-03-01 17:07:46 +0000

    Stop warnings being logged

--- manifests/everyboot.pp
+++ manifests/everyboot.pp
@@ -16,45 +16,47 @@ define outset::everyboot(
             fail('Invalid value for title. Must end in .py, .sh or .rb')
         }
     }
+    # Maybe we're not getting a version. Stops warnings.
+    if $outset_version {
+        if versioncmp($outset_version, '1.0.3') >= 0 {
+            # These were changed in 1.0.3
+            $target = '/usr/local/outset/boot-every'
+        } else {
+            $target = '/usr/local/outset/everyboot-scripts'
+        }
 
-    if versioncmp($outset_version, '1.0.3') >= 0 {
-        # These were changed in 1.0.3
-        $target = '/usr/local/outset/boot-every'
-    } else {
-        $target = '/usr/local/outset/everyboot-scripts'
-    }
+        if $ensure == 'present'{
+            if $type == 'file'{
+                file {"${target}/${priority}-${title}":
+                    source => $script,
+                    owner  => 0,
+                    group  => 0,
+                    mode   => '0755',
+                }
+            }
 
-    if $ensure == 'present'{
-        if $type == 'file'{
+            if $type == 'template'{
+                file {"${target}/${priority}-${title}":
+                    content => $script,
+                    owner  => 0,
+                    group  => 0,
+                    mode   => '0755',
+                }
+            }
+
+            if $immediate_run == true {
+                exec { "${target}/${priority}-${title}":
+                    path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+                    refreshonly => true,
+                    subscribe => File["${target}/${priority}-${title}"],
+                }
+            }
+        }
+
+        if $ensure == 'absent' {
             file {"${target}/${priority}-${title}":
-                source => $script,
-                owner  => 0,
-                group  => 0,
-                mode   => '0755',
+                ensure => absent,
             }
-        }
-
-        if $type == 'template'{
-            file {"${target}/${priority}-${title}":
-                content => $script,
-                owner  => 0,
-                group  => 0,
-                mode   => '0755',
-            }
-        }
-
-        if $immediate_run == true {
-            exec { "${target}/${priority}-${title}":
-                path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-                refreshonly => true,
-                subscribe => File["${target}/${priority}-${title}"],
-            }
-        }
-    }
-
-    if $ensure == 'absent' {
-        file {"${target}/${priority}-${title}":
-            ensure => absent,
         }
     }
 }
